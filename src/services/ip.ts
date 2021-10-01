38576724990c299227d36f2f6688f1d9a8a2da58
m2v <69117810+iuvoJS@users.noreply.github.com>
2021-10-01 22:02:33 +0200

    new ip api

--- src/services/ip.ts
+++ src/services/ip.ts
@@ -8,46 +8,13 @@ export class Ip {
   public async lookup() {
     if (!validator.isIP(this.ip)) {
       return {
-        success: false,
-        message: 'IP is not valid',
+        error: 'IP is not valid',
       };
     }
-    const req = await fetch(
-      `http://ip-api.com/json/${this.ip}?fields=66121727`
-    );
+    const req = await fetch(`https://vpnapi.io/api/${this.ip}?key=${process.env.ipToken}`);
     const json = await req.json();
 
-    return {
-      success: true,
-      query: this.ip,
-      ip: {
-        isV4: isV4Format(this.ip),
-        isV6: isV6Format(this.ip),
-        isPrivate: isPrivate(this.ip),
-      },
-      asn: {
-        as: json.as,
-        asname: json.asname,
-        isp: json.isp,
-        org: json.org,
-      },
-      security: {
-        hosting: false,
-        proxy: false,
-        tor: false,
-        vpn: false,
-      },
-      location: {
-        city: json.city,
-        country: json.country,
-        currency: json.currency,
-        lat: json.lat,
-        lon: json.lon,
-        postal: json.zip,
-        region: json.regionName,
-        timezone: json.timezone,
-      },
-    };
+    return json;
   }
 }
 
